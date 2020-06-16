f89646134154766a189f097be612747fffc23598
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-16 16:32:34 -0700

    modified the handleGetGradesSuccess method

--- components/app.js
+++ components/app.js
@@ -3,9 +3,10 @@ class App {
     console.error(error);
   }
   handleGetGradesSuccess(grades) {
-    console.log(grades);
+    this.gradeTable.updateGrades(grades);
   }
-  constructor() {
+  constructor(gradeTable) {
+    this.gradeTable = gradeTable;
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
   }
