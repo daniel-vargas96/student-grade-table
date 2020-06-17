99a42c8e1877ba0d50a7a5a2522926f705992fcd
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-17 12:12:35 -0700

    modified App class to take 3rd parameter, query form element, and instantiate GradeForm

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var headerEl = document.querySelector("header");
 var tableEl = document.querySelector('table');
+var formEl = document.querySelector("form");
+
 var pageHeader = new PageHeader(headerEl);
 var gradeTable = new GradeTable(tableEl);
-var app = new App(gradeTable, pageHeader);
+var gradeForm = new GradeForm(formEl);
+var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
