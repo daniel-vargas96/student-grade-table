c9a78f6ddd97f02427d88b75e82655fd01df9e31
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-17 12:08:02 -0700

    Added GradeForm class

---
+++ components/grade-form.js
@@ -0,0 +1,14 @@
+class GradeForm {
+  constructor(formElement) {
+    this.handleSubmit.bind(this);
+    this.formElement = formElement;
+    this.formElement.addEventListener('submit', this.handleSubmit);
+  }
+  onSubmit(createGrade) {
+    this.createGrade = createGrade;
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('working');
+  }
+}
