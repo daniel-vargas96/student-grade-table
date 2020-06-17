1e9bd3f491915633fa09a4cac0f28523deba8b3a
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-16 17:34:51 -0700

    queried the header element, instantiated the PageHeader, and modifed App instance to take PageHeader instance as a second argument

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+var headerEl = document.querySelector("header");
 var tableEl = document.querySelector('table');
+var pageHeader = new PageHeader(headerEl);
 var gradeTable = new GradeTable(tableEl);
-var app = new App(gradeTable);
+var app = new App(gradeTable, pageHeader);
 app.start();
