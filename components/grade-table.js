f34f43374733152b1c65fb0f4080de8b27084867
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-16 17:05:09 -0700

    modified updateGrades method to dynamically create table and display student names, courses, and grades

--- components/grade-table.js
+++ components/grade-table.js
@@ -4,6 +4,17 @@ class GradeTable {
     this.updateGrades.bind(this);
   }
   updateGrades(grades) {
-    console.log(grades);
+    var tbody = document.querySelector("tbody");
+    for(var i = 0; i < grades.length; i++) {
+      var row = document.createElement("tr");
+      var studentName = document.createElement("td");
+      var course = document.createElement("td");
+      var studentGrade = document.createElement("td");
+      studentName.textContent = grades[i].name;
+      course.textContent = grades[i].course;
+      studentGrade.textContent = grades[i].grade;
+      row.append(studentName, course, studentGrade);
+      tbody.appendChild(row);
+    }
   }
 }
