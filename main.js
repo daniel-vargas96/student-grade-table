16a67c8231daec1fc58179bcdb3e792e261a3c7c
Daniel Vargas <dannyvargashb@gmail.com>
2020-06-17 15:32:51 -0700

    Enhance the GradeTable Component

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var headerEl = document.querySelector("header");
 var tableEl = document.querySelector('table');
 var formEl = document.querySelector("form");
+var noGradesEl = document.querySelector(".hidden");
 
 var pageHeader = new PageHeader(headerEl);
-var gradeTable = new GradeTable(tableEl);
+var gradeTable = new GradeTable(tableEl, noGradesEl);
 var gradeForm = new GradeForm(formEl);
 var app = new App(gradeTable, pageHeader, gradeForm);
 app.start();
